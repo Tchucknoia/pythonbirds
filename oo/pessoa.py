f4d33e6d07cd86b36d2d4f829ad96fe5ee1782e2
Tchucknoia <tiagolachman@gmail.com>
2020-01-08 17:23:47 -0200

    Criado e removido atributo dinâmico de objetos do tipo Pessoa

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,19 +1,22 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = 18):#esse init vai ser sempre executado quando você criar um objeto
+    def __init__(self, *filhos, nome = None, idade = 18):#esse init vai ser sempre executado quando você criar um objeto
         self.idade = idade
         self.nome = nome
+        self.filhos = list(filhos)
     def cumprimentar(self):
         return f'Salve {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa('Tiago', 18)
-    print(p.nome)
-    print(p.idade)
-    p.nome = 'Biel'
-    print(p.nome)
-    p.idade = 9
-    print(p.idade)
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    tiago = Pessoa(nome='Tiago')
+    andre = Pessoa(tiago, nome='André', idade=40)
+    print(andre.nome)
+    print(andre.idade)
+    print(andre.filhos)#retorna feio pq oq ta dentro da lista filhos é um objeto feito na linha 12
+    for filho in andre.filhos:#pra poder mostrar os nomes dos filhos (sendo todos objetos) faz isso
+        print(filho.nome)
+    print(tiago.__dict__)#__dict__ é um atributo que retorna todos os outros atributos em forma de dicionário
+    tiago.sobrenome = 'Lachman'#atributo dinâmico se adiciona assim e ele SÓ VALE para aquele objeto
+    print(tiago.sobrenome)
+    del tiago.idade#o contrario de adicionar o atributo dinamico é remover atributos dinamicamente
+    print(tiago.__dict__)

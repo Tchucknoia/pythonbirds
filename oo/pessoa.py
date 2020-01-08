d912fd87a1472a5b8b8e82ac2d738221a444fed6
Tchucknoia <tiagolachman@gmail.com>
2020-01-08 16:55:04 -0200

    Criados atributos de instância nome e idade "bagulho do __init__ (o cara que sempre vai se intromete quando criarem um objeto dessa classe)"

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,10 +1,19 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 18):#esse init vai ser sempre executado quando você criar um objeto
+        self.idade = idade
+        self.nome = nome
     def cumprimentar(self):
         return f'Salve {id(self)}'
 
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Tiago', 18)
+    print(p.nome)
+    print(p.idade)
+    p.nome = 'Biel'
+    print(p.nome)
+    p.idade = 9
+    print(p.idade)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
